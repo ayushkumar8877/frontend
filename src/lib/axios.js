78719f3afe4b7952173dfa997291900f33f3e00b
ayushkumar8877 <ayushkumar88770@gmail.com>
2025-07-15 23:39:40 +0530

    Remove .env file from repository

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "production"
     ? "https://backend-0kia.onrender.com/api"
-    : "http://localhost:5000/api";
+    : "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
